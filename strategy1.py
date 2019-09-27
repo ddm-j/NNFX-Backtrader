e7fd6c314297073861d2ee1be66133184a642cba
bjorn <denbjoren505@gmail.com>
2019-09-27 00:34:46 -0600

    first commit

--- strategy1.py
+++ strategy1.py
@@ -1,5 +1,3 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
 import sys
 import datetime  # For datetime objects
 import os.path  # To manage paths
