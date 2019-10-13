53aef91dc967943d0d9f341e940d12f04bc3cf72
HedgeBJohnson <denbjornen505@gmail.com>
2019-10-13 13:42:19 -0600

    Working Genetic Optimization

--- optimizer.py
+++ optimizer.py
@@ -3,15 +3,22 @@ from itertools import product
 from custom_functions import *
 from nnfx import NNFX
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
 import time
+from deap import base, creator, tools
+import random
 
 class Optimization(object):
 
-    def __init__(self,selections):
+    def __init__(self,mode,selections,density,lower,upper):
         self.master = MasterParameters()
         self.ingest = dict()
         self.selections = selections
+        self.density = density
+        self.mode = mode
+        self.lower = lower
+        self.upper = upper
         # Define The Indicators for Optimization
 
     def baseline(self,lower,upper):
@@ -331,17 +338,24 @@ class Optimization(object):
             'conds': None,
         }
 
-    def load_indicators(self,density):
+    def filter_indicators(self):
 
-        self.baseline(8,40)
-        self.confirmation(8,40)
-        self.volume(8,40)
-        self.exit(8,40)
         for typ, ind in self.ingest.items():
             for name, args in ind.items():
                 if name not in self.selections[typ]:
                     continue
-                self.master.add_indicator(typ,name,args['input'],args['conds'],density)
+                if self.mode == 'brute':
+                    self.master.add_indicator(typ,name,args['input'],args['conds'],self.density)
+                elif self.mode == 'genetic':
+                    self.key[name] = args
+
+    def load_indicators(self):
+
+        self.baseline(self.lower,self.upper)
+        self.confirmation(self.lower,self.upper)
+        self.volume(self.lower,self.upper)
+        self.exit(self.lower,self.upper)
+        self.filter_indicators()
 
     def generate_combinations(self):
 
@@ -354,7 +368,7 @@ class Optimization(object):
         combinations  = [x for x in combinations if x[1][0]!=x[2][0]]
         self.combinations = combinations
 
-    def optimize(self):
+    def brute(self):
 
         # Parameter Initialization
 
@@ -415,20 +429,254 @@ class Optimization(object):
         return_opt.to_csv('opt_results.csv')
         print(return_opt)
 
+    def encode_chromosome(self):
+
+        self.key = dict()
+        self.load_indicators()
+        chromo_map = []
+        chromo_range = []
+        chromo_conds = []
+        for i, (ind, args) in enumerate(self.key.items()):
+            for j, (input, params) in enumerate(args.items()):
+                if input == 'input':
+                    for k, param in params.items():
+                        chromo_map.append(param[0])
+                        chromo_range.append(param[1])
+                elif input == 'conds':
+                    if params:
+                        re_index = []
+                        for cond_set in params:
+                            re_index.append([cond_set[0]+i,cond_set[1],cond_set[2]+i])
+                        chromo_conds.append(re_index)
+
+        return chromo_map, chromo_conds, chromo_range
+
+    def decode_chromosome(self,chromo):
+        decoded = []
+        i = 0
+        for j, (ind,args) in enumerate(self.key.items()):
+            params = []
+            for k, (name, gene) in enumerate(args['input'].items()):
+                params.append(chromo[i])
+                i += 1
+            decoded.append([ind,params])
+        return decoded
+
+    def evaluate_pop(self,pop,metric):
+
+        # Create a cerebro entity
+        cerebro = bt.Cerebro(stdstats=False)
+
+        # Enable Slippage on Open Price (Approximately %0.01 percent)
+        cerebro.broker = bt.brokers.BackBroker(slip_perc=0.0001, slip_open=True)
+
+        # Add our strategy
+        cerebro.optstrategy(NNFX, optim=pop)
+
+        # Get Data Files from Data Folder
+        paths, names = file_browser()
+
+        dpath = 'Data/'
+        datasets = [
+            (dpath + path, name) for path, name in zip(paths, names)
+        ]
+
+        trade_pairs = ['EURUSD', 'GBPUSD', 'USDCHF', 'USDJPY']
+        datasets = [i for i in datasets if i[1] in trade_pairs]
+
+        # Create a Data Feeds and Add to Cerebro
+        for i in range(len(datasets)):
+            data = bt.feeds.GenericCSVData(dataname=datasets[i][0],
+                                           openinterest=-1,
+                                           dtformat='%d.%m.%Y %H:%M:%S.000')
+            cerebro.adddata(data, name=datasets[i][1])
+
+        # Set our desired cash start
+        cerebro.broker.setcash(1000.0)
+
+        # Add Analyzers
+        cerebro.addanalyzer(bt.analyzers.SQN, _name="sqn")
+        cerebro.addanalyzer(bt.analyzers.Returns, _name='ret')
+        cerebro.addanalyzer(bt.analyzers.DrawDown, _name='dd')
+
+        res = cerebro.run()
+
+        return_opt = dict()
+        return_opt['params'] = []
+        return_opt['ret'] = []
+        return_opt['sqn'] = []
+        return_opt['dd'] = []
+        return_opt['cal'] = []
+        for r in res:
+            params = r[0].params.optim
+            ret = r[0].analyzers.ret.get_analysis()['rnorm']
+            sqn = r[0].analyzers.sqn.get_analysis()['sqn']
+            dd = r[0].analyzers.dd.get_analysis().max.drawdown
+            try:
+                cal = ret / (dd / 100)
+            except:
+                cal = 'nan'
+            return_opt['params'].append(params)
+            return_opt['ret'].append(ret)
+            return_opt['sqn'].append(sqn)
+            return_opt['dd'].append(dd)
+            return_opt['cal'].append(cal)
+        return return_opt[metric]
+
+    def get_truth(self, inp, relate, cut):
+        ops = {'>': operator.gt,
+               '<': operator.lt,
+               '>=': operator.ge,
+               '<=': operator.le,
+               '=': operator.eq}
+        return ops[relate](inp, cut)
+
+    def checkBounds(self, ranges):
+        def decorator(func):
+            def wrapper(*args, **kargs):
+                offspring = func(*args, **kargs)
+                for child in offspring:
+                    for i in range(len(child)):
+                        if ranges[i] and child[i] > ranges[i][1]:
+                            child[i] = ranges[i][1]
+                        elif ranges[i] and child[i] < ranges[i][0]:
+                            child[i] = ranges[i][0]
+                return offspring
+            return wrapper
+        return decorator
+
+    def genetic(self):
+
+        # Fitness Definition
+        creator.create('FitnessMax', base.Fitness, weights=(1.0,))
+
+        # Individual Definition
+        creator.create('Individual', list, fitness=creator.FitnessMax)
+
+        # Genetic Toolbox
+        toolbox = base.Toolbox()
+
+        # Register the Attributes
+        types, conds, ranges = self.encode_chromosome()
+        attributes = []
+        for i, (t, r) in enumerate(zip(types,ranges)):
+            attributes.append(i)
+            if t == int:
+                toolbox.register(str(i), random.randint, r[0], r[1])
+            elif t == bool:
+                toolbox.register(str(i), random.randint, 0, 1)
+            elif t == float:
+                toolbox.register(str(i), random.uniform, r[0], r[1])
+            elif t == 'custom':
+                toolbox.register(str(i), random.choice, r)
+            else:
+                print('ERROR - NO TYPE DETECTION SETUP')
+        attributes = [getattr(toolbox,str(i)) for i in attributes]
+
+        # Register the Individual
+        toolbox.register("individual", tools.initCycle, creator.Individual, tuple(attributes), n=1)
+
+        # Register the Population
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+        # Register Evalutation
+        toolbox.register("evaluate", self.evaluate_pop)
+
+        # Register Mating
+        toolbox.register("mate", tools.cxTwoPoint)
+        toolbox.decorate("mate", self.checkBounds(ranges))
+
+        # Register Mutation
+        toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+        toolbox.decorate("mutate", self.checkBounds(ranges))
+
+        # Register Selection
+        toolbox.register("select", tools.selTournament, tournsize=3)
+
+        # MAIN ALGORITHM
+        pop = toolbox.population(n=3000)
+        fitnesses = self.evaluate_pop([self.decode_chromosome(i) for i in pop],'ret')
+        for ind, fit in zip(pop, fitnesses):
+            ind.fitness.values = (fit,)
+
+        CXPB, MUTPB = 0.5, 0.2
+
+        # Extracting all the fitnesses
+        fits = [ind.fitness.values[0] for ind in pop]
+
+        # Begin the evolution
+        NGEN = 20
+        for i in tqdm(range(NGEN)):
+            # A new generation
+            print("-- Generation %i --" % i)
+
+            # Select the next generation individuals
+            offspring = toolbox.select(pop, len(pop))
+            # Clone the selected individuals
+            offspring = list(map(toolbox.clone, offspring))
+
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() < CXPB:
+                    toolbox.mate(child1, child2)
+                    del child1.fitness.values
+                    del child2.fitness.values
+
+            for mutant in offspring:
+                if random.random() < MUTPB:
+                    toolbox.mutate(mutant)
+                    del mutant.fitness.values
+
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            for ind in invalid_ind[:]:
+                for i, r in zip(ind,ranges):
+                    if r and (i < r[0] or i > r[1]):
+                        invalid_ind.remove(ind)
+
+            print('Popluation Size: %i'%len(invalid_ind))
+
+            fitnesses = self.evaluate_pop([self.decode_chromosome(i) for i in invalid_ind],'ret')
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = (fit,)
+
+            pop[:] = offspring
+
+            # Gather all the fitnesses in one list and print the stats
+            fits = [ind.fitness.values[0] for ind in pop]
+
+            print('Best Individual')
+            print(self.decode_chromosome(invalid_ind[np.argmax(fitnesses)]))
+
+            length = len(pop)
+            mean = sum(fits) / length
+            sum2 = sum(x * x for x in fits)
+            std = abs(sum2 / length - mean ** 2) ** 0.5
+
+            print("  Min %s" % min(fits))
+            print("  Max %s" % max(fits))
+            print("  Avg %s" % mean)
+            print("  Std %s" % std)
+
+    def optimize(self):
+        if self.mode == 'brute':
+            self.brute()
+        elif self.mode == 'genetic':
+            self.genetic()
 
 if __name__ == '__main__':
 
     #freeze_support()
     # Selection Tests
     selections = {
-        'baseline': ['kijun'],
+        'baseline': ['alaguerre'],
         'confirmation': ['itrend','mama'],
-        'volume': ['tdfi'],
-        'exit': ['ssl']
+        'volume': ['damiani'],
+        'exit': ['dosc']
     }
 
 
-    opt = Optimization(selections)
+    opt = Optimization(mode='genetic',selections=selections,density=5,lower=8,upper=60)
     opt.optimize()
 
 
